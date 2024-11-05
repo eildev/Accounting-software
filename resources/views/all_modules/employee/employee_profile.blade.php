286333993aaf09c5fa21db09120a268b10aa910b
NayemHassan <nayem15-12498@diu.edu.bd>
2024-11-05 17:55:41 +0600

    profile

--- resources/views/all_modules/employee/employee_profile.blade.php
+++ resources/views/all_modules/employee/employee_profile.blade.php
@@ -11,19 +11,14 @@
                     <div
                         class="d-flex justify-content-between align-items-center position-absolute top-90 w-100 px-2 px-md-4 mt-n4">
                         <div>
-                            <img class="wd-70 rounded-circle" src="{{ asset('uploads/employee/' . $employee->pic) }}" alt="profile">
-                            <span class="h4 ms-3 text-dark">{{$employee->full_name}}</span>
+                            <img class="wd-70 rounded-circle" src="{{ asset('uploads/employee/' . $employee->pic) }}"
+                                alt="profile">
+                            <span class="h4 ms-3 text-dark">{{ $employee->full_name }}</span>
                         </div>
-                        {{-- <div class="d-none d-md-block">
-                            <button class="btn btn-primary btn-icon-text">
-                                <i data-feather="edit" class="btn-icon-prepend"></i> Edit profile
-                            </button>
-                        </div> --}}
                     </div>
                 </div>
                 <div class="d-flex justify-content-center p-3 rounded-bottom">
                     <ul class="d-flex align-items-center m-0 p-0 nav nav-tabs" id="myTab" role="tablist">
-
                         <li class="d-flex align-items-center active">
                             <a class="pt-1px d-none d-md-block text-primary nav-link active" id="Home-tab"
                                 data-bs-toggle="tab" href="#home" role="tab" aria-controls="home"
@@ -40,7 +35,6 @@
                                     class="text-muted tx-12">3,765</span></a>
                         </li>
 
-                        </li>
                     </ul>
                 </div>
             </div>
@@ -53,23 +47,23 @@
                 <div class="card-body text-center">
                     <div class="d-flex align-items-center  text-center justify-content-between mb-2">
                         <h4 class="card-title mb-0  text-center">Name</h4>
-                        <h6>{{$employee->full_name ?? '-'}}</h6>
+                        <h6>{{ $employee->full_name ?? '-' }}</h6>
                     </div>
                     <div class="d-flex align-items-center  text-center justify-content-between mb-2">
                         <h4 class="card-title mb-0  text-center">Phone</h4>
-                        <h6>{{$employee->phone ?? '-'}}</h6>
+                        <h6>{{ $employee->phone ?? '-' }}</h6>
                     </div>
                     <div class="d-flex align-items-center  text-center justify-content-between mb-2">
                         <h4 class="card-title mb-0  text-center">Email</h4>
-                        <h6>{{$employee->email ?? '-'}}</h6>
+                        <h6>{{ $employee->email ?? '-' }}</h6>
                     </div>
                     <div class="d-flex align-items-center  text-center justify-content-between mb-2">
                         <h4 class="card-title mb-0  text-center">Designation </h4>
-                        <h6>{{$employee->designation ?? '-'}}</h6>
+                        <h6>{{ $employee->designation ?? '-' }}</h6>
                     </div>
                     <div class="d-flex align-items-center  text-center justify-content-between mb-2">
                         <h4 class="card-title mb-0  text-center">Address</h4>
-                        <h6>{{$employee->address ?? '-'}}</h6>
+                        <h6>{{ $employee->address ?? '-' }}</h6>
                     </div>
                 </div>
             </div>
@@ -81,50 +75,41 @@
             <div class="row">
                 <div class="col-md-12 grid-margin ">
                     <div class="tab-content">
-                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
-                        <div class="card rounded">
-                            <div class="card-body">
-                                <p class="mb-3 tx-14">Salary Structure</p>
-                                <table class="table table-bordered">
-                                    <thead>
-                                        <tr>
-                                            <th>Salary</th>
-                                            <th>House Rent</th>
-                                            <th>Transport Allowance</th>
-                                            <th>Deductions</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-
+                        <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                            <div class="card rounded">
+                                <div class="card-body">
+                                    <p class="mb-3 tx-14">Salary Structure</p>
+                                    <table class="table table-bordered">
+                                        <thead>
                                             <tr>
-                                                <td>{{$salaryStructure->base_salary ?? '-'}}</td>
-                                                <td>{{$salaryStructure->house_rent ?? '-'}}</td>
-                                                <td>{{$salaryStructure->transport_allowance ?? '-'}}</td>
-                                                <td>{{$salaryStructure->deductions ?? '-'}}</td>
+                                                <th>Salary</th>
+                                                <th>House Rent</th>
+                                                <th>Transport Allowance</th>
+                                                <th>Deductions</th>
                                             </tr>
-
-                                    </tbody>
-                                </table>
-
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td>{{ $salaryStructure->base_salary ?? '-' }}</td>
+                                                <td>{{ $salaryStructure->house_rent ?? '-' }}</td>
+                                                <td>{{ $salaryStructure->transport_allowance ?? '-' }}</td>
+                                                <td>{{ $salaryStructure->deductions ?? '-' }}</td>
+                                            </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="tab-pane fade " id="about" role="tabpanel" aria-labelledby="about-tab">
+                            <div class="card rounded">
+                                <div class="card-body">
+                                    <p class="mb-3 tx-14">About</p>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="tab-pane fade " id="about" role="tabpanel" aria-labelledby="about-tab">
-                        <div class="card rounded">
-                            <div class="card-body">
-                                <p class="mb-3 tx-14">About</p>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
                 </div>
             </div>
-
         </div>
-
-    </div>
-    <!-- middle wrapper end -->
-
     </div>
 @endsection
