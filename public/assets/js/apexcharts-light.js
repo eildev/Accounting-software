5d16120f1fc1ca541545ed93f68f407ae21b3201
NayemHassan <nayem15-12498@diu.edu.bd>
2024-11-19 17:58:00 +0600

    dashboard bonus dynamic worked

--- public/assets/js/apexcharts-light.js
+++ public/assets/js/apexcharts-light.js
@@ -127,7 +127,7 @@ $(function() {
       tooltip: {
         theme: 'light'
       },
-      colors: [colors.primary],    
+      colors: [colors.primary],
       grid: {
         padding: {
           bottom: -4
@@ -172,7 +172,7 @@ $(function() {
         }
       }
     }
-    
+
     var apexBarChart = new ApexCharts(document.querySelector("#apexBar"), options);
     apexBarChart.render();
   }
@@ -414,51 +414,51 @@ $(function() {
 
 
   // Apex Donut chart start
-  if ($('#apexDonut').length) {
-    var options = {
-      chart: {
-        height: 300,
-        type: "donut",
-        foreColor: colors.bodyColor,
-        background: colors.cardBg,
-        toolbar: {
-          show: false
-        },
-      },
-      theme: {
-        mode: 'light'
-      },
-      tooltip: {
-        theme: 'light'
-      },
-      stroke: {
-        colors: ['rgba(0,0,0,0)']
-      },
-      colors: [colors.primary,colors.warning,colors.danger, colors.info],
-      legend: {
-        show: true,
-        position: "top",
-        horizontalAlign: 'center',
-        fontFamily: fontFamily,
-        itemMargin: {
-          horizontal: 8,
-          vertical: 0
-        },
-      },
-      dataLabels: {
-        enabled: false
-      },
-      series: [44, 55, 13, 33]
-    };
+//   if ($('#apexDonut').length) {
+//     var options = {
+//       chart: {
+//         height: 300,
+//         type: "donut",
+//         foreColor: colors.bodyColor,
+//         background: colors.cardBg,
+//         toolbar: {
+//           show: false
+//         },
+//       },
+//       theme: {
+//         mode: 'light'
+//       },
+//       tooltip: {
+//         theme: 'light'
+//       },
+//       stroke: {
+//         colors: ['rgba(0,0,0,0)']
+//       },
+//       colors: [colors.primary,colors.warning,colors.danger, colors.info],
+//       legend: {
+//         show: true,
+//         position: "top",
+//         horizontalAlign: 'center',
+//         fontFamily: fontFamily,
+//         itemMargin: {
+//           horizontal: 8,
+//           vertical: 0
+//         },
+//       },
+//       dataLabels: {
+//         enabled: false
+//       },
+//       series: [44, 55, 13, 33]
+//     };
 
-    var chart = new ApexCharts(document.querySelector("#apexDonut"), options);
-    chart.render();
-  }
+//     var chart = new ApexCharts(document.querySelector("#apexDonut"), options);
+//     chart.render();
+//   }
   // Apex Donut chart start
 
 
 
-  
+
   // Apex Pie chart end
   if ($('#apexPie').length) {
     var options = {
@@ -496,9 +496,9 @@ $(function() {
       },
       series: [44, 55, 13, 33]
     };
-    
+
     var chart = new ApexCharts(document.querySelector("#apexPie"), options);
-    chart.render();  
+    chart.render();
   }
   // Apex Pie chart end
 
@@ -750,7 +750,7 @@ $(function() {
 
 
 
-  
+
   // Apex Scatter chart start
   if ($('#apexScatter').length) {
     var options = {
@@ -863,7 +863,7 @@ $(function() {
       },
       colors: [colors.primary, colors.warning, colors.danger, colors.info],
       fill: {
-        
+
       },
       grid: {
         padding: {
@@ -884,7 +884,7 @@ $(function() {
             background: colors.gridBorder,
             strokeWidth: '100%',
             opacity: 1,
-            margin: 5, 
+            margin: 5,
         },
         }
       },
@@ -901,11 +901,11 @@ $(function() {
         },
       },
     };
-    
+
     var chart = new ApexCharts(document.querySelector("#apexRadialBar"), options);
     chart.render();
     var chartAreaBounds = chart.w.globals.dom.baseEl.querySelector('.apexcharts-inner').getBoundingClientRect();
   }
-  // Apex Radialbar chart end  
+  // Apex Radialbar chart end
 
 });
